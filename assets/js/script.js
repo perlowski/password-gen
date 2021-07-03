02091c8aa76fbd8a6e7fce1ec688d121f752e18a
Perlowski <perlowskistephen@gmail.com>
2021-07-02 21:50:13 -0500

    create variables, arrays, passwordfunction, and start if loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
-  
-// Assignment code here
+  // Assignment code here
 
-window.alert("Hello World")
+  // variables
+var charLength;
+var confirmUpper;
+var confirmLower;
+var confirmNumber;
+var confirmSpecial;
+
+upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+specials = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "?"]
+
+
+function generatePassword() {
+   charLength = prompt("How many characters would you like your password to be? (from 8-128)")
+    if (!charLength) {
+        alert("Enter a number.")
+        generatePassword()
+    }
+    else if (charLength < 8 || charLength > 128) {
+        alert("Must be from 8-128!")
+        generatePassword()
+    }
+    else  {
+       confirmUpper = confirm("Would you like your password to contain uppercase letters?")
+       confirmLower = confirm("Would you like your password to contain lowercase letters?")
+       confirmNumber = confirm("Would you like your password to contain numbers?")
+       confirmSpecial = confirm("Would you like your password to contain special characters?")
+    }
+    if (!confirmUpper && !confirmLower && !confirmNumber && !confirmSpecial) {
+        choices = alert("You must select at least one of the criteria!")
+    }
+
+}
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
